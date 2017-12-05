64b5e7a54e4b221f7c9b2b6ac20f865659d55cac
Your name <Your email address>
2017-12-05 18:07:48 +0000

    Added JS to contact form

---
+++ js/contact_form.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){ 
+    $('#characterLeft').text('*240 characters left');
+    $('#message').keydown(function () {
+        var max = 240;
+        var len = $(this).val().length;
+        if (len >= max) {
+            $('#characterLeft').text('*You have reached the limit');
+            $('#characterLeft').addClass('red');
+            $('#submit').addClass('disabled');            
+        } 
+        else {
+            var ch = max - len;
+            $('#characterLeft').text(ch + ' characters left');
+            $('#submit').removeClass('disabled');
+            $('#characterLeft').removeClass('red');            
+        }
+    });
+    
+    
+});
+
+function greeting(form) {
+     
+    alert("Thank you! We have received your message and will contact shortly!");
+    return true;
+    }
